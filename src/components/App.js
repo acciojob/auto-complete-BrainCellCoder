92f3e96cd49373626a2435ff2f68475e92ed276b
BrainCellCoder <00abhisheksharma1999@gmail.com>
2024-10-07 09:14:23 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-
-import React from "react";
-import './../styles/App.css';
-
+import React, { useState } from "react";
+import "./../styles/App.css";
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
 const App = () => {
+  const [filterdFruits, setFilteredFruits] = useState(fruits);
+
+  const handleInput = (e) => {
+    const newArr = fruits.filter((fruit) => fruit.includes(e.target.value));
+    setFilteredFruits(newArr);
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <input type="text" onChange={(e) => handleInput(e)} />
+      <ul>
+        {filterdFruits.map((fruit, index) => {
+          return <li key={index}>{fruit}</li>;
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
